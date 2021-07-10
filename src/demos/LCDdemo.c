10673a4feeba5b28a7e0226e0566b57a6fc7bfc4
Austin Welch <66348244+AustinWelch@users.noreply.github.com>
2021-07-10 19:52:25 -0400

    LCD Demo that displays distances traveled

--- src/demos/LCDdemo.c
+++ src/demos/LCDdemo.c
@@ -4,23 +4,21 @@
 #include "driverlib.h"
 #include <fpu.h>
 #include <stdint.h>
+#include "MotorControl.h"
 
 // Graphic library context
 Graphics_Context g_sContext;
 
-void clockInit(void);
+char* output[100];
+uint32_t distance = 0;
 
+void displayDistance(void);
 int main(){
 
     BSP_InitBoard();
     FPU_enableModule();
-    clockInit();
+    Motor_initialize();
     __enable_interrupt();
-
-    int16_t ulIdx;
-    Graphics_Rectangle myRectangle1 = { 10, 50, 155, 120};
-    Graphics_Rectangle myRectangle2 = { 150, 100, 300, 200};
-    Graphics_Rectangle myRectangle3 = { 0, 0, 319, 239};
 
     Kitronix320x240x16_SSD2119Init();
     Graphics_initContext(&g_sContext, &g_sKitronix320x240x16_SSD2119);
@@ -28,32 +26,63 @@ int main(){
     Graphics_setFont(&g_sContext, &g_sFontCmss20b);
     Graphics_clearDisplay(&g_sContext);
 
-    Graphics_drawPixel(&g_sContext, 45, 45);
-    Graphics_fillRectangle(&g_sContext, &myRectangle1);
-    Graphics_drawPixel(&g_sContext, 45, 50);
-    Graphics_drawPixel(&g_sContext, 50, 50);
-    Graphics_drawPixel(&g_sContext, 50, 45);
-    Graphics_drawLine(&g_sContext, 60, 60, 200, 200);
-    Graphics_drawLine(&g_sContext, 30, 200, 200, 60);
-    Graphics_drawLine(&g_sContext, 0, Graphics_getDisplayHeight(
-                          &g_sContext) - 1,
-                      Graphics_getDisplayWidth(&g_sContext) - 1,
-                      Graphics_getDisplayHeight(&g_sContext) - 1);
+    displayDistance();
 
-    while(1);
+    while(1){
+
+        Odometer_start();
+
+        Motor_moveForwardDistance(50, 10);
+        displayDistance();
+        DelayMs(500);
+
+        Motor_moveForwardDistance(50, 20);
+        displayDistance();
+        DelayMs(500);
+
+        Motor_moveForwardDistance(50, 30);
+        displayDistance();
+        DelayMs(500);
+
+        Motor_moveForwardDistance(50, 40);
+        displayDistance();
+        DelayMs(500);
+
+        Motor_moveForwardDistance(50, 50);
+        displayDistance();
+        DelayMs(500);
+
+        Motor_moveForwardDistance(50, 60);
+       displayDistance();
+       DelayMs(500);
+
+       Motor_moveForwardDistance(50, 70);
+       displayDistance();
+       DelayMs(500);
+
+       Motor_moveForwardDistance(50, 80);
+       displayDistance();
+       DelayMs(500);
+
+       Motor_moveForwardDistance(50, 90);
+       displayDistance();
+       DelayMs(500);
+
+       Motor_moveForwardDistance(50, 100);
+       displayDistance();
+       DelayMs(500);
+
+    }
 }
 
-void clockInit(void)
-{
-    /* 2 flash wait states, VCORE = 1, running off DC-DC, 48 MHz */
-    FlashCtl_setWaitState(FLASH_BANK0, 2);
-    FlashCtl_setWaitState(FLASH_BANK1, 2);
-    PCM_setPowerState(PCM_AM_DCDC_VCORE1);
-    CS_setDCOCenteredFrequency(CS_DCO_FREQUENCY_48);
-    CS_setDCOFrequency(48000000);
-    CS_initClockSignal(CS_MCLK, CS_DCOCLK_SELECT, 1);
-    CS_initClockSignal(CS_SMCLK, CS_DCOCLK_SELECT, 1);
-    CS_initClockSignal(CS_HSMCLK, CS_DCOCLK_SELECT, 1);
+void displayDistance(){
+    sprintf(output, "Distance Traveled: %i.%i", distance/10000, (distance/1000) % 10);
+    Graphics_setForegroundColor(&g_sContext, GRAPHICS_COLOR_WHITE);
+    Graphics_drawStringCentered(&g_sContext, output, AUTO_STRING_LENGTH, 159, 15, TRANSPARENT_TEXT);
 
-    return;
+    distance = Odometer_value();
+    sprintf(output, "Distance Traveled: %i.%i", distance/10000, (distance/1000) % 10);
+    Graphics_setForegroundColor(&g_sContext, GRAPHICS_COLOR_BLACK);
+    Graphics_drawStringCentered(&g_sContext, output, AUTO_STRING_LENGTH, 159, 15, TRANSPARENT_TEXT);
+    Odometer_clear();
 }
