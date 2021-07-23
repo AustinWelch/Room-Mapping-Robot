456750bd5c39269802817b5fa8445713819481cf
Austin Welch <66348244+AustinWelch@users.noreply.github.com>
2021-07-22 20:59:24 -0400

    Rid of externs

--- RobotLibrary/Motors/Tachometer.h
+++ RobotLibrary/Motors/Tachometer.h
@@ -1,8 +1,8 @@
 #include "BSP.h"
 
-extern void Tachometer_init();
-extern void Tachometer_leftMotorISR();
-extern void Tachometer_rightMotorISR();
-extern void Tachometer_disable();
-extern void Tachometer_odometerEnabled();
+void Tachometer_init();
+void Tachometer_leftMotorISR();
+void Tachometer_rightMotorISR();
+void Tachometer_disable();
+void Tachometer_odometerEnabled();
 void Tachometer_countToTravel(uint32_t count);
